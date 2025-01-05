aa9a1cd882f7d4ef80521a95f6e97290335c109f
Seth Martineau <martineausw@gmail.com>
2025-01-04 19:03:22 -0500

    Open file

--- course-notes.py
+++ course-notes.py
@@ -1,1 +1,16 @@
-print("Hello world")
+notes_structure = open("sample-course", "r")
+notes_lines = notes_structure.readlines()
+
+institution_name = notes_lines[0].rstrip()
+course_code = notes_lines[1].split()[0].strip()
+course_name = notes_lines[1][len(course_code) + 1:].strip()
+
+print(institution_name, course_code, course_name)
+
+
+"""
+for line in notes_lines:
+    
+"""    
+
+notes_structure.close()
